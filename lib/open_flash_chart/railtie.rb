8a0ac8cfcd2677aa120144349f7e7fa547c08af7
Siarhei Kavaliou <siarhei_kavaliou@epam.com>
2014-01-14 08:18:45 +0000

    Changed initializer

--- lib/open_flash_chart/railtie.rb
+++ lib/open_flash_chart/railtie.rb
@@ -1,11 +1,11 @@
 module OpenFlashChart
   class Railtie < ::Rails::Railtie
     initializer 'open_flash_chart' do |_app|
-      ActiveSupport.on_load(:active_record) do
-        ::ActiveRecord::Base.send :include, ::OpenFlashChart::View
-        ::ActiveRecord::Base.send :include, ::OpenFlashChart::Controller
-        ::ActiveRecord::Base.send :include, ::OpenFlashChart
-      end
+#      ActiveSupport.on_load(:active_record) do
+#        ::ActiveRecord::Base.send :include, ::OpenFlashChart::View
+#        ::ActiveRecord::Base.send :include, ::OpenFlashChart::Controller
+#        ::ActiveRecord::Base.send :include, ::OpenFlashChart
+#      end
       ActiveSupport.on_load(:action_view) do
         ::ActionView::Base.send :include, ::OpenFlashChart::View
         ::OpenFlashChart::Base.send :include, ::OpenFlashChart::View
